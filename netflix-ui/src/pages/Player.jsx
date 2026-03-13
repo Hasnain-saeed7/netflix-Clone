23717055b452fd898e7f5fe4cf3e80869eac8004
Hasnain Saeed <hasnainqureshi2232@gmail.com>
2026-03-13 17:54:15 +0500

    feat: replaced local video with Cloudinary URL and optimized project size

--- netflix-ui/src/pages/Player.jsx
+++ netflix-ui/src/pages/Player.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { BsArrowLeft } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
-import video from "../assets/video.mp4"; 
 import Footer from "../components/Footer";
 export default function Player() {
   const navigate = useNavigate();
